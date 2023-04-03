0c9b38247858261e3547480201ff0f8f25612747
Volodymyr Atamanenko <v@atamanenko.in>
2023-04-03 17:28:21 +0400

    Make the logger safer and lighter (no std-lib dep)

--- FalsoJNI_Logger.c
+++ FalsoJNI_Logger.c
@@ -3,7 +3,7 @@
  *
  * Fake Java Native Interface, providing JavaVM and JNIEnv objects.
  *
- * Copyright (C) 2022 Volodymyr Atamanenko
+ * Copyright (C) 2022-2023 Volodymyr Atamanenko
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -17,7 +17,8 @@
 #include "FalsoJNI_Logger.h"
 #include "FalsoJNI.h"
 
-pthread_mutex_t * _fjni_log_mutex = NULL;
+#include <psp2/kernel/clib.h>
+#include <psp2/kernel/threadmgr.h>
 
 #define COLOR_RED     "\x1B[31m"
 #define COLOR_ORANGE  "\x1B[33m"
@@ -25,84 +26,79 @@ pthread_mutex_t * _fjni_log_mutex = NULL;
 
 #define COLOR_END     "\033[0m"
 
+static SceKernelLwMutexWork _fjni_log_mutex;
+static volatile short int _fjni_log_mutex_inited = 0;
 
-void _fjni_log_lock() {
-    if (_fjni_log_mutex == NULL) {
-        pthread_mutex_t initTmpNormal;
-        _fjni_log_mutex = malloc(sizeof(pthread_mutex_t));
-        memcpy(_fjni_log_mutex, &initTmpNormal, sizeof(pthread_mutex_t));
+static char _fjni_log_buffer_1[2048];
+static char _fjni_log_buffer_2[2048];
 
-        if (pthread_mutex_init(_fjni_log_mutex, NULL) != 0) {
-            _fjni_log_mutex = NULL;
-            return;
-        }
+#define LOG_LOCK \
+    if (!_fjni_log_mutex_inited) { \
+        int ret = sceKernelCreateLwMutex(&_fjni_log_mutex, "fjni_log_lock", 0, 0, NULL); \
+        if (ret < 0) { \
+            sceClibPrintf("[JNI] Error: failed to create log mutex: 0x%x\n", ret); \
+            return; \
+        } \
+        _fjni_log_mutex_inited = 1; \
+    } \
+    sceKernelLockLwMutex(&_fjni_log_mutex, 1, NULL);
+
+#define LOG_UNLOCK \
+    if (_fjni_log_mutex_inited) { \
+        sceKernelUnlockLwMutex(&_fjni_log_mutex, 1); \
     }
 
-    pthread_mutex_lock(_fjni_log_mutex);
-}
+#define LOG_PRINT \
+    va_list list; \
+    va_start(list, fmt); \
+    sceClibVsnprintf(_fjni_log_buffer_2, sizeof(_fjni_log_buffer_2), _fjni_log_buffer_1, list); \
+    va_end(list); \
+    sceClibPrintf(_fjni_log_buffer_2);
 
-void _fjni_log_unlock() {
-    if (_fjni_log_mutex) {
-        pthread_mutex_unlock(_fjni_log_mutex);
-    }
-}
-
-int _fjni_log_info(const char *fname, int lineno, const char *fxname, const char* fmt, ...) {
+void _fjni_log_info(const char *fi, int li, const char *fn, const char* fmt, ...) {
 #if FALSOJNI_DEBUGLEVEL <= FALSOJNI_DEBUG_INFO
-    _fjni_log_lock();
-    char fmt_colored[2048];
-    snprintf(fmt_colored, 2047, "%s[INFO] %s%s\n", COLOR_BLUE, fmt, COLOR_END);
+    LOG_LOCK
 
-    va_list list;
-    va_start(list, fmt);
-    vfprintf(stderr, fmt_colored, list);
-    va_end(list);
-    _fjni_log_unlock();
+    sceClibSnprintf(_fjni_log_buffer_1, sizeof(_fjni_log_buffer_1),
+                    "%s[INFO] %s%s\n", COLOR_BLUE, fmt, COLOR_END);
+
+    LOG_PRINT
+    LOG_UNLOCK
 #endif
-    return 0;
 }
 
-int _fjni_log_warn(const char *fname, int lineno, const char *fxname, const char* fmt, ...) {
+void _fjni_log_warn(const char *fi, int li, const char *fn, const char* fmt, ...) {
 #if FALSOJNI_DEBUGLEVEL <= FALSOJNI_DEBUG_WARN
-    _fjni_log_lock();
-    char fmt_colored[2048];
-    snprintf(fmt_colored, 2047, "%s[WARNING][%s:%d][%s] %s%s\n", COLOR_ORANGE, fname, lineno, fxname, fmt, COLOR_END);
+    LOG_LOCK
 
-    va_list list;
-    va_start(list, fmt);
-    vfprintf(stderr, fmt_colored, list);
-    va_end(list);
-    _fjni_log_unlock();
+    sceClibSnprintf(_fjni_log_buffer_1, sizeof(_fjni_log_buffer_1),
+                    "%s[WARN][%s:%d][%s] %s%s\n", COLOR_ORANGE, fi, li, fn, fmt, COLOR_END);
+
+    LOG_PRINT
+    LOG_UNLOCK
 #endif
-    return 0;
 }
 
-int _fjni_log_debug(const char *fname, int lineno, const char *fxname, const char* fmt, ...) {
+void _fjni_log_debug(const char *fi, int li, const char *fn, const char* fmt, ...) {
 #if FALSOJNI_DEBUGLEVEL <= FALSOJNI_DEBUG_ALL
-    _fjni_log_lock();
-    char fmt_real[4096];
-    snprintf(fmt_real, 2047, "[DEBUG][%s:%d][%s] %s\n", fname, lineno, fxname, fmt);
+    LOG_LOCK
 
-    va_list list;
-    va_start(list, fmt);
-    vfprintf(stderr, fmt_real, list);
-    va_end(list);
-    _fjni_log_unlock();
+    sceClibSnprintf(_fjni_log_buffer_1, sizeof(_fjni_log_buffer_1),
+                    "[DBG][%s:%d][%s] %s\n", fi, li, fn, fmt);
+
+    LOG_PRINT
+    LOG_UNLOCK
 #endif
-    return 0;
 }
 
-int _fjni_log_error(const char *fname, int lineno, const char *fxname, const char* fmt, ...) {
+void _fjni_log_error(const char *fi, int li, const char *fn, const char* fmt, ...) {
 #if FALSOJNI_DEBUGLEVEL <= FALSOJNI_DEBUG_ERROR
-    _fjni_log_lock();
-    char fmt_colored[2048];
-    snprintf(fmt_colored, 2047, "%s[ERROR][%s:%d][%s] %s%s\n", COLOR_RED, fname, lineno, fxname, fmt, COLOR_END);
+    LOG_LOCK
 
-    va_list list;
-    va_start(list, fmt);
-    vfprintf(stderr, fmt_colored, list);
-    va_end(list);
-    _fjni_log_unlock();
+    sceClibSnprintf(_fjni_log_buffer_1, sizeof(_fjni_log_buffer_1),
+                    "%s[ERROR][%s:%d][%s] %s%s\n", COLOR_RED, fi, li, fn, fmt, COLOR_END);
+
+    LOG_PRINT
+    LOG_UNLOCK
 #endif
-    return 0;
 }
--- FalsoJNI_Logger.h
+++ FalsoJNI_Logger.h
@@ -3,7 +3,7 @@
  *
  * Fake Java Native Interface, providing JavaVM and JNIEnv objects.
  *
- * Copyright (C) 2022 Volodymyr Atamanenko
+ * Copyright (C) 2022-2023 Volodymyr Atamanenko
  *
  * This software may be modified and distributed under the terms
  * of the MIT license. See the LICENSE file for details.
@@ -21,20 +21,20 @@
 extern "C" {
 #endif
 
-#define fjni_logv_info(fmt, ...)   _fjni_log_info(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
-#define fjni_logv_warn(fmt, ...)   _fjni_log_warn(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
-#define fjni_logv_dbg(fmt, ...)  _fjni_log_debug(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
-#define fjni_logv_err(fmt, ...)  _fjni_log_error(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
+#define fjni_logv_info(fmt, ...)  _fjni_log_info(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
+#define fjni_logv_warn(fmt, ...)  _fjni_log_warn(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
+#define fjni_logv_dbg(fmt, ...)   _fjni_log_debug(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
+#define fjni_logv_err(fmt, ...)   _fjni_log_error(__FILE__, __LINE__, __func__, fmt, __VA_ARGS__)
 
-#define fjni_log_info(fmt)     _fjni_log_info(__FILE__, __LINE__, __func__, fmt)
-#define fjni_log_warn(fmt)     _fjni_log_warn(__FILE__, __LINE__, __func__, fmt)
-#define fjni_log_dbg(fmt)     _fjni_log_debug(__FILE__, __LINE__, __func__, fmt)
-#define fjni_log_err(fmt)     _fjni_log_error(__FILE__, __LINE__, __func__, fmt)
+#define fjni_log_info(fmt)        _fjni_log_info(__FILE__, __LINE__, __func__, fmt)
+#define fjni_log_warn(fmt)        _fjni_log_warn(__FILE__, __LINE__, __func__, fmt)
+#define fjni_log_dbg(fmt)         _fjni_log_debug(__FILE__, __LINE__, __func__, fmt)
+#define fjni_log_err(fmt)         _fjni_log_error(__FILE__, __LINE__, __func__, fmt)
 
-int _fjni_log_info(const char *fname, int lineno, const char *fxname, const char* fmt, ...);
-int _fjni_log_warn(const char *fname, int lineno, const char *fxname, const char* fmt, ...);
-int _fjni_log_debug(const char *fname, int lineno, const char *fxname, const char* fmt, ...);
-int _fjni_log_error(const char *fname, int lineno, const char *fxname, const char* fmt, ...);
+void _fjni_log_info(const char *fi, int li, const char *fn, const char* fmt, ...);
+void _fjni_log_warn(const char *fi, int li, const char *fn, const char* fmt, ...);
+void _fjni_log_debug(const char *fi, int li, const char *fn, const char* fmt, ...);
+void _fjni_log_error(const char *fi, int li, const char *fn, const char* fmt, ...);
 
 #ifdef __cplusplus
 };
